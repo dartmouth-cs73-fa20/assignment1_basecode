404f9ce128e80ba0cd02f80a9b350322ee1c69db
LocalLemma <50967166+LocalLemma@users.noreply.github.com>
2020-09-13 20:23:23 -0400

    Update a1_main.cpp

--- src/a1_main.cpp
+++ src/a1_main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 // This is a way for you to test your functions. 
-// We will only grade the contents of a1.cpp and floatimage.cpp
+// We will only grade the contents of a1.cpp and array3D.h
 int main()
 {
     cout << "Congratulations, you have compiled the starter code!" << endl;
